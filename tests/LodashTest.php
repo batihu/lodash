6e0be940df4360b35b45e6cbedd2ccfd5bba8a46
bierbaum.attila <bierbaum.attila@virgo.hu>
2016-12-05 20:02:03 +0100

    refact

--- tests/LodashTest.php
+++ tests/LodashTest.php
@@ -6,20 +6,25 @@ use Lodash\Lodash;
 
 class LodashTest extends \PHPUnit_Framework_TestCase
 {
+    public $l;
+
+    public function __construct()
+    {
+        $this->l = new Lodash();
+    }
+
 
     public function testChunk()
     {
-        $l =new Lodash();
-        $this->assertEquals( [['a', 'b'], ['c', 'd']] , $l->chunk(['a', 'b', 'c', 'd'], 2));
-        $this->assertEquals( [['a', 'b', 'c'], ['d']] , $l->chunk(['a', 'b', 'c', 'd'], 3));
+        $this->assertEquals([['a', 'b'], ['c', 'd']], $this->l->chunk(['a', 'b', 'c', 'd'], 2));
+        $this->assertEquals([['a', 'b', 'c'], ['d']], $this->l->chunk(['a', 'b', 'c', 'd'], 3));
     }
 
 
     public function testCompact()
     {
-        $l = new Lodash();
-        $this->assertEquals([1,2,3], $l->compact([0, 1, false, 2, '', 3]));
-        $this->assertEquals([4,5,6,7], $l->compact([4,false,5,0,6,'',7,false, false, '', ]));
-        $this->assertEquals([4,5,6,7], $l->compact([6,false,4,0,7,'',5,false, false, '', ]));
+        $this->assertEquals([1, 2, 3], $this->l->compact([0, 1, false, 2, '', 3]));
+        $this->assertEquals([4, 5, 6, 7], $this->l->compact([4, false, 5, 0, 6, '', 7, false, false, '',]));
+        $this->assertEquals([4, 5, 6, 7], $this->l->compact([6, false, 4, 0, 7, '', 5, false, false, '',]));
     }
 }
